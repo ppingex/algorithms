bd8bd795f9dd45304a86a64e2268e3daa69ebf80
angelina <27081989adel@gmail.com>
2021-02-01 16:49:09 +0300

    all files added

---
+++ алгорим форда фалкерсона/алгорим форда фалкерсона/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace алгорим_форда_фалкерсона
+{
+    public class MaxFlow
+    {
+        static readonly int V = 7; //Количество вершин в графе
+        bool bfs(int[,] rGraph, int s, int t, int[] parent)  // Возвращает true, если есть путь из S в T в остаточном графе
+        {
+            bool[] visited = new bool[V];
+            for (int i = 0; i < V; ++i)
+                visited[i] = false;
+
+            List<int> queue = new List<int>();   // Создаём список, помещаем туда стоковую вершинy и помечаем её как посещённую
+            queue.Add(s);
+            visited[s] = true;
+            parent[s] = -1;
+
+            while (queue.Count != 0) // Поиск в ширину
+            {
+                int u = queue[0];
+                queue.RemoveAt(0);
+                for (int v = 0; v < V; v++)
+                {
+                    if (visited[v] == false && rGraph[u, v] > 0)
+                    {
+                        queue.Add(v);
+                        parent[v] = u;
+                        visited[v] = true;
+                    }
+                }
+            }
+            return (visited[t] == true);
+        }
+
+        int FordFulkerson(int[,] graph, int s, int t) 
+        {
+            int u, v;
+            int[,] rGraph = new int[V, V]; 
+            for (u = 0; u < V; u++)
+                for (v = 0; v < V; v++)
+                    rGraph[u, v] = graph[u, v];
+            int[] parent = new int[V];  
+            int max_flow = 0; 
+            while (bfs(rGraph, s, t, parent))   // пока есть путь из истока в сток
+            {
+                int path_flow = int.MaxValue;
+                for (v = t; v != s; v = parent[v])    // Находим мин пропускную способность по ребру и пропускаем по нему макс поток
+                {
+                    u = parent[v];
+                    path_flow = Math.Min(path_flow, rGraph[u, v]);
+                }
+                for (v = t; v != s; v = parent[v])    // Обновляем остаточные мощности
+                {
+                    u = parent[v];
+                    rGraph[u, v] -= path_flow;
+                    rGraph[v, u] += path_flow;
+                }
+                max_flow += path_flow;
+            }
+            return max_flow;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int[,] graph = new int[7, 7]
+                    { { 0, 2, 0, 7, 5, 0, 0},
+                    { 0, 0, 4, 0, 0, 0, 0},
+                    { 0, 0, 0, 0, 0, 0, 7},
+                    { 0, 6, 9, 0, 0, 1, 1},
+                    { 0, 0, 0, 8, 0, 6, 0},
+                    { 0, 0, 0, 0, 0, 0, 8},
+                    { 0, 0, 0, 0, 0, 0, 0} };
+                MaxFlow m = new MaxFlow();
+                Console.WriteLine("Максимальный поток = " + m.FordFulkerson(graph, 0, 6));
+            }
+        }
+    }
+}
---
+++ алгоритм басакера гоуэна/алгоритм басакера гоуэна/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace basaker
+{
+    class Program
+    {
+        const int potok = 6;
+        const int V = 7; //количество вершин в графе
+        static int[,] bandwidth; //матрица пропускных способностей
+        static int[,] cost; //матрица стоимостей
+        static int[,] flow; //матрица пропущенного потока
+        static void Main(string[] args)
+        {
+            bandwidth = new int[,] { {0, 4, 0, 4, 3, 0, 0},
+                                     {0, 0, 0, 0, 0, 0, 3},
+                                     {0, 4, 0, 0, 0, 0, 4},
+                                     {0, 0, 3, 0, 0, 2, 0},
+                                     {0, 0, 0, 0, 0, 4, 0},
+                                     {0, 0, 0, 0, 0, 0, 3},
+                                     {0, 0, 0, 0, 0, 0, 0} };
+
+            cost = new int[,] { {0, 2, 0, 1, 1, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 4},
+                                {0, 2, 0, 0, 0, 0, 1},
+                                {0, 0, 2, 0, 0, 3, 0},
+                                {0, 0, 0, 0, 0, 2, 0},
+                                {0, 0, 0, 0, 0, 0, 1},
+                                {0, 0, 0, 0, 0, 0, 0} };
+            Basaker(potok);
+        }
+        public static void Basaker(int potok)
+        {
+            flow = new int[V, V];
+            for (int i = 0; i < V; i++)
+            {
+                for (int j = 0; j < V; j++)
+                {
+                    flow[i, j] = 0;
+                }
+            }
+            List<int> way = new List<int>();
+            do
+            {
+                int currentFlow = 0;
+                int[,] modcost = new int[V, V];
+                for (int i = 0; i < V; i++)
+                {
+                    for (int j = 0; j < V; j++)
+                    {
+                        if (flow[i, j] == 0 && modcost[i, j] == 0) //если поток по ребру равен 0, оставляем без изменений
+                        {
+                            modcost[i, j] = cost[i, j];
+                        }
+                        else if (flow[i, j] == bandwidth[i, j] && modcost[i, j] == 0) //если поток по ребру максимальный, изменяем направлене ребра и изменяем знак стоимости
+                        {
+                            modcost[j, i] = -cost[i, j];
+                        }
+                        else if (flow[i, j] < bandwidth[i, j] && modcost[i, j] == 0) //если поток по ребру не максимальный, добавляем обратное ребро с отрицательной стоимостью
+                        {
+                            modcost[i, j] = cost[i, j];
+                            modcost[j, i] = -cost[i, j];
+                        }
+                    }
+                }
+                way = FloydWarshall(modcost); //поиск пути и запись его в список
+                UpFlow(way); // насыщение потока
+                for (int j = 0; j < V; j++)
+                {
+                    currentFlow += flow[0, j];
+                }
+                if (currentFlow >= potok)
+                {
+                    OutPutFlow(currentFlow); // вывод текущего потока
+                    OveralCost(); //вывод стоимости
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Текущий поток {currentFlow} < {potok} => продолжаем");
+                    Console.WriteLine();
+                }
+            } while (way.Count != 0);
+        }
+
+        public static void OutPutFlow(int currentFlow)
+        {
+            Console.WriteLine("Текущий поток на сети равен {0}", currentFlow);
+        }
+
+        public static void OveralCost()
+        {
+            int ocost = 0;
+            for (int i = 0; i < V; i++)
+            {
+                for (int j = 0; j < V; j++)
+                {
+                    ocost += flow[i, j] * cost[i, j];
+                }
+            }
+            Console.WriteLine("Стоимость пути равна {0}", ocost);
+        }
+
+        public static void UpFlow(List<int> way) //насыщение потока
+        {
+            int maxflow = 1000; //полагаем минимальный допустимый пропускаемый поток бесконечности
+            for (int i = 0; i < way.Count - 1; i++)
+            {
+                if (bandwidth[way[i], way[i + 1]] == 0) //если нет пропущенного потока, то берем его f
+                {
+                    if (maxflow > flow[way[i + 1], way[i]])
+                    {
+                        Console.WriteLine("{0} -> {1} = {2}", way[i] + 1, way[i + 1] + 1, flow[way[i + 1], way[i]]);
+                    }
+                }
+                else if (maxflow > bandwidth[way[i], way[i + 1]] - flow[way[i], way[i + 1]]) //если есть пропущенный поток, то берем f-c
+                {
+                    Console.WriteLine("{0} -> {1} = {2}", way[i] + 1, way[i + 1] + 1, bandwidth[way[i], way[i + 1]] - flow[way[i], way[i + 1]]);
+                }
+            }
+            for (int i = 0; i < way.Count - 1; i++)
+            {
+                if (bandwidth[way[i], way[i + 1]] == 0) //если нет пропущенного потока, то берем его f
+                {
+                    if (maxflow > flow[way[i + 1], way[i]])
+                    {
+                        maxflow = flow[way[i + 1], way[i]];
+                    }
+                }
+                else if (maxflow > bandwidth[way[i], way[i + 1]] - flow[way[i], way[i + 1]]) //если есть пропущенный поток, то берем f-c
+                {
+                    maxflow = bandwidth[way[i], way[i + 1]] - flow[way[i], way[i + 1]];
+                }
+            }
+            if (maxflow != 1000)
+            {
+                Console.WriteLine("Насыщаем поток на {0}", maxflow);
+            }
+
+            for (int i = 0; i < way.Count - 1; i++)
+            {
+                if (bandwidth[way[i], way[i + 1]] != 0) //если ребро прямое прибавляем минимальный поток
+                {
+                    flow[way[i], way[i + 1]] += maxflow;
+                }
+                else //иначе отнимаем минимальный поток
+                {
+                    flow[way[i + 1], way[i]] -= maxflow;
+                }
+            }
+        }
+        public static List<int> FloydWarshall(int[,] modcost) //Нахождение кратчайшего пути Методом Флойда-Уоршелла
+        {
+            List<int> way = new List<int>();
+            int[,] distance = new int[V, V];
+            int[,] costF = new int[V, V];
+
+            for (int i = 0; i < V; ++i)
+            {
+                for (int j = 0; j < V; ++j)
+                {
+                    if (modcost[i, j] == 0)
+                    {
+                        distance[i, j] = 1000;
+                    }
+                    else
+                    {
+
+                        distance[i, j] = modcost[i, j];
+                    }
+
+                }
+            }
+            for (int i = 0; i < V; i++)
+            {
+                for (int j = 0; j < V; j++)
+                {
+                    costF[i, j] = j;
+                }
+            }
+            for (int k = 0; k < V; ++k)
+            {
+                for (int i = 0; i < V; ++i)
+                {
+                    for (int j = 0; j < V; ++j)
+                    {
+
+                        if (distance[i, k] + distance[k, j] < distance[i, j])
+                        {
+                            distance[i, j] = distance[i, k] + distance[k, j];
+                            costF[i, j] = costF[i, k];
+                        }
+                    }
+                }
+            }
+
+            if (distance[0, V - 1] != 1000)
+            {
+                Console.WriteLine("Минимальный путь равен {0}", distance[0, V - 1]);
+                way = FindWay(costF, distance);
+                Console.WriteLine();
+            }
+            return way;
+        }
+
+        public static List<int> FindWay(int[,] costF, int[,] distance)
+        {
+            int currentNode = 0;
+            List<int> way = new List<int>();
+            way.Add(0);
+            while (currentNode != V - 1)
+            {
+                way.Add(costF[currentNode, V - 1]);
+                currentNode = costF[currentNode, V - 1];
+            }
+            foreach (int node in way)
+            {
+                Console.Write("{0} ", node + 1);
+            }
+            return way;
+        }
+    }
+}
